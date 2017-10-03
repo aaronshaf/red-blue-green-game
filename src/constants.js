7d6f3953b6a895e506f91c19ff677405ec72c8f8
Aaron Shafovaloff <aaronshaf@gmail.com>
2017-10-03 09:54:54 -0600

    WIP

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,5 @@ export const COLLISION_PROXIMITY = 3
 export const INTERVAL = 25
 export const MIN_SPEED = 0.25
 export const MAX_SPEED = 20
-export const MAX_BLUE_SPEED = 2
+export const MAX_BLUE_SPEED = 1
 export const MAX_RED_SPEED = 2
